44d5b8b8d0844159ffe6fca99dae61decfaa5578
Henri Ly <henri@softlydev.com>
2020-09-14 15:39:49 +0200

    [Polkadot] allow new account to be displayed in sync

--- src/api/Polkadot.js
+++ src/api/Polkadot.js
@@ -1,3 +1,5 @@
+// @flow
+
 import network from "../network";
 import { BigNumber } from "bignumber.js";
 import { getEnv } from "../env";
@@ -15,47 +17,60 @@ async function fetch(url: string) {
 
 export const getBalances = async (addr: string) => {
   const url = `${getBaseApiUrl()}/polkadot/api/v1/account/${addr}`;
-  console.log(url);
-  const { data } = await fetch(url);
 
-  return {
-    balance: BigNumber(data.attributes.balance_total),
-    spendableBalance: BigNumber(data.attributes.balance_free),
-    // delegated: data.attributes.balance_reserved,
-    polkadotResources: { nonce: data.attributes.nonce },
-  };
-};
-
-export const getTransfers = async (accountId: string, addr: string) => {
-  const page = 1;
-
-  let url = `${getBaseApiUrl()}/polkadot/api/v1/balances/transfer?filter[address]=${addr}&page[number]=${page}&page[size]=100`;
-  console.log(url);
-  let data = await fetch(url);
-
-  console.log("getTransfers", data);
-
-  const operations = data.data.map((op) => {
-    const type =
-      op.attributes.event_id === "Reward"
-        ? "IN"
-        : op.attributes.sender.attributes.address === addr
-        ? "OUT"
-        : "IN";
+  try {
+    const { data } = await fetch(url);
 
     return {
-      id: `${accountId}-${op.attributes.event_idx}-${type}`,
-      accountId,
-      type,
-      value: BigNumber(op.attributes.value),
-      hash: op.attributes.event_idx,
-      fee: BigNumber(op.attributes.fee),
-      senders: [op.attributes.sender.attributes.address],
-      recipients: [op.attributes.destination.attributes.address],
-      blockHeight: op.attributes.block_id,
-      date: new Date(),
+      balance: BigNumber(data.attributes.balance_total),
+      spendableBalance: BigNumber(data.attributes.balance_free),
+      // delegated: data.attributes.balance_reserved,
+      polkadotResources: { nonce: data.attributes.nonce },
     };
-  });
+  } catch (e) {
+    return {
+      balance: BigNumber(0),
+      spendableBalance: BigNumber(0),
+      polkadotResources: { nonce: 0 },
+    };
+  }
+};
+
+// Only get the first 100 operations,
+// we probably are going to change indexer because this one missing some important information like date
+// It's just a semi-mock to be able to go forward on the code
+export const getTransfers = async (accountId: string, addr: string) => {
+  const page = 1;
+  let operations = [];
+
+  let url = `${getBaseApiUrl()}/polkadot/api/v1/balances/transfer?filter[address]=${addr}&page[number]=${page}&page[size]=100`;
+  try {
+    let data = await fetch(url);
+
+    operations = data.data.map((op) => {
+      const type =
+        op.attributes.event_id === "Reward"
+          ? "IN"
+          : op.attributes.sender.attributes.address === addr
+          ? "OUT"
+          : "IN";
+
+      return {
+        id: `${accountId}-${op.attributes.event_idx}-${type}`,
+        accountId,
+        type,
+        value: BigNumber(op.attributes.value),
+        hash: op.attributes.event_idx,
+        fee: BigNumber(op.attributes.fee),
+        senders: [op.attributes.sender.attributes.address],
+        recipients: [op.attributes.destination.attributes.address],
+        blockHeight: op.attributes.block_id,
+        date: new Date(),
+      };
+    });
+  } catch (e) {
+    operations = [];
+  }
 
   return operations;
 };
--- src/families/polkadot/hw-getAddress.js
+++ src/families/polkadot/hw-getAddress.js
@@ -2,25 +2,18 @@
 
 import type { Resolver } from "../../hw/getAddress/types";
 import Polkadot from "./ledger-app/Polkadot";
-import { withLibcore } from "../../libcore/access";
 import BIPPath from "bip32-path";
 
 const resolver: Resolver = async (transport, { path, verify }) => {
   const polkadot = Polkadot.newPolkadotApp(transport);
 
-  console.log("polkadotPath", path);
   const bipPath = BIPPath.fromString(path).toPathArray();
-
-  console.log(bipPath);
-
   const r = await polkadot.getAddress(
     bipPath[2],
     bipPath[3],
     bipPath[4],
     verify
   );
-
-  console.log(r);
 
   return {
     address: r.address,
