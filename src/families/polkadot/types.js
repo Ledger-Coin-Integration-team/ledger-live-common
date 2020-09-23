1a7565fdbd8e5fda0be2af17c64e554538009d0b
Henri Ly <henri@softlydev.com>
2020-09-23 16:01:36 +0200

    [Polkadot] Types draft

--- src/families/polkadot/types.js
+++ src/families/polkadot/types.js
@@ -6,6 +6,8 @@ import type {
   TransactionCommonRaw,
 } from "../../types/transaction";
 
+export type PolkadotOperationMode = "send" | "bond" | "nominate" | "unbond"
+
 export type CoreStatics = {};
 
 export type CoreAccountSpecifics = {};
@@ -16,20 +18,42 @@ export type CoreCurrencySpecifics = {};
 
 export type NetworkInfo = {|
   family: "polkadot",
+  fees: BigNumber
 |};
 
 export type NetworkInfoRaw = {|
   family: "polkadot",
+  fees: string
 |};
+
+export type PolkadotValidator = {|
+  address: string,
+  rewards: BigNumber,
+  isElected: boolean,
+|}
+
+export type PolkadotValidatorRaw = {|
+  address: string,
+  rewards: BigNumber,
+  isElected: boolean,
+|}
+
+export type PolkadotUnbondings = {|
+  amount: BigNumber,
+  completionDate: Date
+|}
 
 export type PolkadotResources = {|
   nonce: number,
   bondedBalance: BigNumber,
+  unbondings: PolkadotUnbondings[],
+  validators: PolkadotValidator,
 |};
 
 export type PolkadotResourcesRaw = {|
   nonce: number,
   bondedBalance: string,
+  validators: ?PolkadotValidatorRaw;
 |};
 
 export type Transaction = {|
@@ -46,6 +70,30 @@ export type TransactionRaw = {|
   mode: string,
   networkInfo: ?NetworkInfoRaw,
   validators?: string[]
+|};
+
+export type PolkadotValidatorItem = {|
+  validatorAddress: string,
+  identity: string,
+  isOversubscribed: boolean,
+  nominatorCount: number,
+  rewardsPoint: number,
+  commission: number,
+  totalBonded: BigNumber,
+  selfBonded: BigNumber,
+  status: "active" | "inactive" | "waiting"
+|};
+
+export type PolkadotValidatorItemRaw = {|
+  validatorAddress: string,
+  identity: string,
+  isOversubscribed: boolean,
+  nominatorCount: number,
+  rewardsPoint: number,
+  commission: number,
+  totalBonded: string,
+  selfBonded: string,
+  status: "active" | "inactive" | "waiting"
 |};
 
 export const reflect = (_declare: *) => {};
