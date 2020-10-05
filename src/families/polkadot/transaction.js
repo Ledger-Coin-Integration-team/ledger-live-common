7a1f29757d52d297df2474f83eb88ab6ebadbe4e
Henri Ly <henri@softlydev.com>
2020-10-05 19:35:53 +0200

    [Polkadot] get transaction status wip

---
+++ src/families/polkadot/transaction.js
@@ -0,0 +1,54 @@
+// @flow
+import { BigNumber } from "bignumber.js";
+import type { Transaction, TransactionRaw } from "./types";
+import {
+  fromTransactionCommonRaw,
+  toTransactionCommonRaw,
+} from "../../transaction/common";
+import type { Account } from "../../types";
+import { getAccountUnit } from "../../account";
+import { formatCurrencyUnit } from "../../currencies";
+
+export const formatTransaction = (
+  { mode, amount, fees, recipient, validators, useAllAmount }: Transaction,
+  account: Account
+): string =>
+  `
+${mode.toUpperCase()} ${
+    useAllAmount
+      ? "MAX"
+      : amount.isZero()
+      ? ""
+      : " " +
+        formatCurrencyUnit(getAccountUnit(account), amount, {
+          showCode: true,
+          disableRounding: true,
+        })
+  }
+TO ${recipient}
+${!validators ? "" : validators.join("\n")}
+with fees=${fees ? formatCurrencyUnit(getAccountUnit(account), fees) : "?"}`;
+
+export const fromTransactionRaw = (tr: TransactionRaw): Transaction => {
+  const common = fromTransactionCommonRaw(tr);
+  return {
+    ...common,
+    family: tr.family,
+    mode: tr.mode,
+    fees: tr.fees ? BigNumber(tr.fees) : null,
+    validators: tr.validators,
+  };
+};
+
+export const toTransactionRaw = (t: Transaction): TransactionRaw => {
+  const common = toTransactionCommonRaw(t);
+  return {
+    ...common,
+    family: t.family,
+    mode: t.mode,
+    fees: t.fees?.toString(),
+    validators: t.validators,
+  };
+};
+
+export default { formatTransaction, fromTransactionRaw, toTransactionRaw };
