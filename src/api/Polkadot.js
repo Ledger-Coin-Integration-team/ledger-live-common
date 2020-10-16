7ddd5b92d3bd04cfaa453e7e3df68b7f314d1f17
Rémi Jarasson (Ext) <remi.jarasson-ext@ledger.fr>
2020-10-16 11:01:35 +0200

    refacto(polkadot): use WS api client only

--- src/api/Polkadot.js
+++ src/api/Polkadot.js
@@ -1,154 +1,207 @@
 // @flow
-
 import network from "../network";
 import { BigNumber } from "bignumber.js";
 import { getEnv } from "../env";
 import { WsProvider, ApiPromise } from "@polkadot/api";
-import { getRegistry } from "@substrate/txwrapper";
-import type { Account } from "../types";
+
+type AsyncApiFunction = (api: typeof ApiPromise) => Promise<any>;
 
 const getBaseApiUrl = () => getEnv("API_POLKADOT_INDEXER");
-const getRpcUrl = () => getEnv("API_POLKADOT_NODE");
+const getWsUrl = () => getEnv("API_POLKADOT_NODE");
 
-async function fetch(url: string) {
-  const { data } = await network({
-    method: "GET",
-    url,
-  });
-
-  return data;
-}
-
-async function getApi() {
-  const wsProvider = new WsProvider("ws://localhost:9944");
+/**
+ * Connects to Substrate Node, executes calls then disconnects
+ *
+ * @param {*} execute - the calls to execute on api
+ */
+async function withApi(execute: AsyncApiFunction): Promise<any> {
+  const wsProvider = new WsProvider(getWsUrl());
   const api = await ApiPromise.create({ provider: wsProvider });
 
-  await api.isReady;
-  return api;
+  try {
+    await api.isReady;
+    const res = await execute(api);
+
+    return res;
+  } finally {
+    await api.disconnect();
+  }
 }
 
-const rpc = async (method: string, params: any[] = []): Promise<any> => {
-  const {
-    data: { result, error },
-  } = await network({
-    url: getRpcUrl(),
-    method: "POST",
-    data: JSON.stringify({
-      id: 1,
-      jsonrpc: "2.0",
-      method,
-      params,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
+/**
+ * Returns true if ElectionStatus is Close. If ElectionStatus is Open, some features must be disabled.
+ */
+export const isElectionClosed = async () =>
+  withApi(async (api: typeof ApiPromise) => {
+    const status = await api.query.staking.eraElectionStatus();
+
+    const res = status.isClose;
+
+    return !!res;
   });
 
-  if (error) {
-    throw new Error(
-      `${error.code} ${error.message}: ${JSON.stringify(error.data)}`
-    );
-  }
+/**
+ * Returns true if an address is valid
+ *
+ * @param {string} addr - the address to test
+ */
+export const isAddressValid = async (addr: string) =>
+  withApi(async (api: typeof ApiPromise) => {
+    try {
+      await api.query.balances.account(addr);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  });
 
-  return result;
-};
+/**
+ * WIP - Returns all the balances for an account
+ *
+ * @param {*} addr - the account address
+ */
+export const getBalances = async (addr: string) =>
+  withApi(async (api: typeof ApiPromise) => {
+    const [allBalances, unbondings, bonded] = await Promise.all([
+      api.derive.balances.all(addr),
+      api.query.staking.ledger(addr),
+      api.query.staking.bonded(addr),
+    ]);
+    const json = JSON.parse(JSON.stringify(allBalances, null, 2));
 
-export const ApigetBalances = async (addr: string) => {
-  const url = `${getBaseApiUrl()}/polkadot/api/v1/account/${addr}`;
-
-  console.log("===== GET BALANCES");
-  try {
-    const { data } = await fetch(url);
-
-    console.log(data);
+    const unbondJson = JSON.parse(JSON.stringify(unbondings, null, 2));
+    const stash = unbondJson ? unbondJson.stash : null;
+    const controller = bonded.isSome ? bonded.unwrap().toString() : null;
 
     return {
-      balance: BigNumber(data.attributes.balance_total || 0),
-      spendableBalance: BigNumber(data.attributes.balance_free || 0),
-      // delegated: data.attributes.balance_reserved,
-      polkadotResources: { nonce: data.attributes.nonce || 0 },
+      balance: BigNumber(json.freeBalance),
+      spendableBalance: BigNumber(json.availableBalance),
+      polkadotResources: {
+        controller,
+        stash,
+        nonce: json.accountNonce,
+        bondedBalance: BigNumber(json.lockedBalance),
+        unbondings: unbondJson
+          ? unbondJson.unlocking.map((unbond) => ({
+              amount: BigNumber(unbond.value),
+              completionDate: new Date(),
+            }))
+          : [], // TODO
+        nominations: [], // TODO
+      },
     };
-  } catch (e) {
-    console.log(e);
+  });
+
+/**
+ * Returns all the params from the chain to build an extrinsic (a transaction on Substrate)
+ */
+export const getTransactionParams = async () =>
+  withApi(async (api: typeof ApiPromise) => {
+    const chainName = await api.rpc.system.chain();
+    const blockHash = await api.rpc.chain.getFinalizedHead();
+    const genesisHash = await api.rpc.chain.getBlockHash(0);
+    const { number } = await api.rpc.chain.getHeader(blockHash);
+    const metadataRpc = await api.rpc.state.getMetadata(blockHash);
+    const {
+      specName,
+      specVersion,
+      transactionVersion,
+    } = await api.rpc.state.getRuntimeVersion(blockHash);
+
     return {
-      balance: BigNumber(0),
-      spendableBalance: BigNumber(0),
-      polkadotResources: { nonce: 0, controller: null },
+      blockHash,
+      blockNumber: number,
+      genesisHash,
+      chainName: chainName.toString(),
+      specName: specName.toString(),
+      specVersion,
+      transactionVersion,
+      metadataRpc,
     };
-  }
-};
+  });
 
-export const getElectionStatus = async () => {
-  const api = await getApi();
+/**
+ * List all validators for the current era, and their exposure.
+ */
+export const getValidators = async () =>
+  withApi(async (api: typeof ApiPromise) => {
+    const validators = await api.query.session.validators();
 
-  const status = await api.query.staking.eraElectionStatus();
+    let formattedValidator = [];
+    if (validators && validators.length > 0) {
+      // Retrieve the balances for all validators
+      const validatorBalances = await Promise.all(
+        validators.map((authorityId) => ({
+          balances: api.query.system.account(authorityId),
+          bonded: api.query.staking.bonded(authorityId),
+        }))
+      );
 
-  const res = status.isClose;
+      formattedValidator = validators.map((authorityId, index) => ({
+        address: authorityId.toString(),
+        balance: validatorBalances[index].balances.data.free.toHuman(),
+        nonce: validatorBalances[index].balances.nonce.toHuman(),
+        bonded: validatorBalances[index].bonded,
+      }));
+    }
 
-  await api.disconnect();
+    return formattedValidator;
+  });
 
-  return res;
-};
+/**
+ * The broadcast function on Substrate
+ *
+ * @param {string} extrinsic - the encoded extrinsic to send
+ */
+export const submitExtrinsic = async (extrinsic: string) =>
+  withApi(async (api: typeof ApiPromise) => {
+    const tx = api.tx(extrinsic);
+    const hash = await api.rpc.author.submitExtrinsic(tx);
 
-export const isAddressValid = async (addr: string) => {
-  const api = await getApi();
+    console.log("broadcast", hash);
 
-  try {
-    await api.query.balances.account(addr);
-  } catch (error) {
-    await api.disconnect();
-    return false;
-  }
+    return hash;
+  });
 
-  await api.disconnect();
-  return true;
-};
+/**
+ * Retrieve the transaction fees and weights
+ * Note: fees on Substrate are not set by the signer, but directly by the blockchain runtime.
+ *
+ * @param {string} extrinsic - the encoded extrinsic to send with a fake signing
+ */
+export const paymentInfo = async (extrinsic: string) =>
+  withApi(async (api: typeof ApiPromise) => {
+    const paymentInfo = await api.rpc.payment.queryInfo(extrinsic);
 
-export const getBalances = async (addr: string) => {
-  const api = await getApi();
+    console.log("paymentInfo", paymentInfo);
 
-  const [allBalances, unbondings, bonded] = await Promise.all([
-    api.derive.balances.all(addr),
-    api.query.staking.ledger(addr),
-    api.query.staking.bonded(addr),
-  ]);
-  const json = JSON.parse(JSON.stringify(allBalances, null, 2));
+    return paymentInfo;
+  });
 
-  const unbondJson = JSON.parse(JSON.stringify(unbondings, null, 2));
-  const stash = unbondJson ? unbondJson.stash : null;
-  const controller = bonded.isSome ? bonded.unwrap().toString() : null;
+/*
+ * EXPLORER/INDEXER FEATURES
+ */
 
-  await api.disconnect();
+/**
+ * WIP - Fetch all operations for an account from Polkascan.
+ * Only get the first 100 operations,
+ * we probably are going to change indexer because this one missing some important information like date
+ * It's just a semi-mock to be able to go forward on the code
+ *
+ * @param {string} accountId - the internal identifier for account
+ * @param {string} addr - the account address on Substrate
+ */
 
-  return {
-    balance: BigNumber(json.freeBalance),
-    spendableBalance: BigNumber(json.availableBalance),
-    polkadotResources: {
-      controller,
-      stash,
-      nonce: json.accountNonce,
-      bondedBalance: BigNumber(json.lockedBalance),
-      unbondings: unbondJson
-        ? unbondJson.unlocking.map((unbond) => ({
-            amount: BigNumber(unbond.value),
-            completionDate: new Date(),
-          }))
-        : [], // TODO
-      nominations: [], // TODO
-    },
-  };
-};
-
-// Only get the first 100 operations,
-// we probably are going to change indexer because this one missing some important information like date
-// It's just a semi-mock to be able to go forward on the code
 export const getTransfers = async (accountId: string, addr: string) => {
   const page = 1;
   let operations = [];
 
   let url = `${getBaseApiUrl()}/polkadot/api/v1/balances/transfer?filter[address]=${addr}&page[number]=${page}&page[size]=100`;
   try {
-    let data = await fetch(url);
+    const { data } = await network({
+      method: "GET",
+      url,
+    });
 
     operations = data.data.map((op) => {
       const type =
@@ -176,110 +229,4 @@ export const getTransfers = async (accountId: string, addr: string) => {
   }
 
   return operations;
-};
-
-/*
- * RPC calls
- */
-
-export const getTransactionParams = async () => {
-  const { block } = await rpc("chain_getBlock");
-  const blockHash = await rpc("chain_getBlockHash");
-  const genesisHash = await rpc("chain_getBlockHash", [0]);
-  const metadataRpc = await rpc("state_getMetadata");
-  const { specVersion, transactionVersion } = await rpc(
-    "state_getRuntimeVersion"
-  );
-
-  return {
-    blockHash,
-    blockNumber: block.header.number,
-    genesisHash,
-    metadataRpc,
-    specVersion,
-    transactionVersion,
-  };
-};
-
-export const getValidators = async () => {
-  const api = await getApi();
-
-  const validators = await api.query.session.validators();
-
-  let formattedValidator = [];
-  if (validators && validators.length > 0) {
-    // Retrieve the balances for all validators
-    const validatorBalances = await Promise.all(
-      validators.map((authorityId) => ({
-        balances: api.query.system.account(authorityId),
-        bonded: api.query.staking.bonded(authorityId),
-      }))
-    );
-
-    formattedValidator = validators.map((authorityId, index) => ({
-      address: authorityId.toString(),
-      balance: validatorBalances[index].balances.data.free.toHuman(),
-      nonce: validatorBalances[index].balances.nonce.toHuman(),
-      bonded: validatorBalances[index].bonded,
-    }));
-  }
-
-  await api.disconnect();
-
-  return formattedValidator;
-};
-
-export const submitExtrinsic = async (extrinsic: string) => {
-  const res = await rpc("author_submitExtrinsic", [extrinsic]);
-
-  console.log("broadcast", res);
-
-  return res;
-};
-
-export const paymentInfo = async (extrinsic: string) => {
-  const res = await rpc("payment_queryInfo", [extrinsic]);
-
-  console.log("paymentInfo", res);
-
-  return res;
-};
-
-const ERA_PERIOD = 64; // number of blocks from checkpoint that transaction is valid
-
-const getNonce = (a: Account) => {
-  return (a.polkadotResources?.nonce || 0) + a.pendingOperations.length;
-};
-
-export const getTxInfo = async (a: Account) => {
-  const {
-    blockHash,
-    blockNumber,
-    genesisHash,
-    metadataRpc,
-    specVersion,
-    transactionVersion,
-  } = await getTransactionParams();
-
-  const registry = getRegistry("Polkadot", "polkadot", specVersion);
-
-  const txBaseInfo = {
-    address: a.freshAddress,
-    blockHash,
-    blockNumber,
-    genesisHash,
-    metadataRpc,
-    nonce: getNonce(a),
-    specVersion,
-    tip: 0,
-    eraPeriod: ERA_PERIOD,
-    transactionVersion,
-  };
-
-  const txOptions = {
-    metadataRpc,
-    registry,
-  };
-
-  return { txBaseInfo, txOptions };
 };
