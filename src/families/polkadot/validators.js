3748b960689c618bdcde9e5d44e590024a1923a2
Henri Ly <henri@softlydev.com>
2020-11-20 16:54:25 +0100

    COINTEMP-395 lint fix

--- src/families/polkadot/validators.js
+++ src/families/polkadot/validators.js
@@ -5,7 +5,8 @@ import { makeLRUCache } from "../../cache";
 
 const cacheValidators = makeLRUCache(
   async (stashes: string | string[]) => await fetchValidators(stashes),
-  (stashes: string | string[]) => (Array.isArray(stashes)) ? stashes.join("-") : stashes 
+  (stashes: string | string[]) =>
+    Array.isArray(stashes) ? stashes.join("-") : stashes
 );
 
 export const getValidators = async (stashes: string | string[]) => {
