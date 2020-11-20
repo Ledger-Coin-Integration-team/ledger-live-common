fbad49f3c0676538ff6b7f142ab647f6714e6d6d
Henri Ly <henri@softlydev.com>
2020-11-20 16:48:25 +0100

    COINTEMP-395 fix type for getValidators

--- src/families/polkadot/validators.js
+++ src/families/polkadot/validators.js
@@ -4,10 +4,10 @@ import { fetchValidators } from "../../api/polkadot";
 import { makeLRUCache } from "../../cache";
 
 const cacheValidators = makeLRUCache(
-  async (stashes: string) => await fetchValidators(stashes),
-  () => ""
+  async (stashes: string | string[]) => await fetchValidators(stashes),
+  (stashes: string | string[]) => (Array.isArray(stashes)) ? stashes.join("-") : stashes 
 );
 
-export const getValidators = async (stashes: string) => {
+export const getValidators = async (stashes: string | string[]) => {
   return await cacheValidators(stashes);
 };
