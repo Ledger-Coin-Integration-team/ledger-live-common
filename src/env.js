de45ed56509e4ab6a6a7d1bb0d304ee9486db986
Henri Ly <henri@softlydev.com>
2020-11-26 19:12:07 +0100

    [Polkadot] fix reward number / fix env bisontrails

--- src/env.js
+++ src/env.js
@@ -59,7 +59,8 @@ const envDefinitions = {
   },
   API_POLKADOT_INDEXER: {
     parser: stringParser,
-    def: "https://polkadot.subscan.io",
+    def: "https://polkadot.indexer.dev.stagebison.net",
+    // def: "https://polkadot.subscan.io",
     desc: "explorer API for polkadot",
   },
   API_POLKADOT_NODE: {
