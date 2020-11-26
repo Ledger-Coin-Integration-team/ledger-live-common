5afc32422c76284b164ed51d501ff416297435fd
henri-ly <33897120+henri-ly@users.noreply.github.com>
2020-11-26 17:13:50 +0100

    [Polkadot] Add poc speculos test (#21)
    * [Polkadot] Add poc speculos test
    
    * [Polkadot] update snapshot + loggier
    
    * [Polkadot] remove unlockings fields

---
+++ src/families/polkadot/speculos-deviceActions.js
@@ -0,0 +1,56 @@
+// @flow
+import type { DeviceAction } from "../../bot/types";
+import type { Transaction } from "./types";
+import { deviceActionFlow } from "../../bot/specs";
+
+const acceptTransaction: DeviceAction<Transaction, *> = deviceActionFlow({
+  steps: [
+    {
+      title: "Balances",
+      button: "Rr",
+    },
+    {
+      title: "Dest",
+      button: "Rr",
+      expectedValue: ({ transaction }) => transaction.recipient,
+    },
+    {
+      title: "Value",
+      button: "Rr",
+      // expectedValue: ({transaction}) => To redo when nano is update today formating is weird
+    },
+    {
+      title: "Chain",
+      button: "Rr",
+      expectedValue: () => "Polkadot",
+    },
+    {
+      title: "Nonce",
+      button: "Rr",
+      expectedValue: ({ account }) =>
+        (account.polkadotResources?.nonce || 0).toString(),
+    },
+    {
+      title: "Tip",
+      button: "Rr",
+    },
+    {
+      title: "Era Phase",
+      button: "Rr",
+    },
+    {
+      title: "Era Period",
+      button: "Rr",
+    },
+    {
+      title: "Block",
+      button: "Rr",
+    },
+    {
+      title: "Approve",
+      button: "LRlr",
+    },
+  ],
+});
+
+export default { acceptTransaction };
