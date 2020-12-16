381356e72f98d8728f9112ef49b2288f31237727
Henri Ly <henri@softlydev.com>
2020-12-16 17:05:26 +0100

    [Polkadot] feedback test

--- src/families/polkadot/synchronisation.js
+++ src/families/polkadot/synchronisation.js
@@ -8,7 +8,9 @@ import { getAccount, getOperations } from "../../api/polkadot";
 export const getAccountShape: GetAccountShape = async (info) => {
   const { id, address, initialAccount } = info;
   const oldOperations = initialAccount?.operations || [];
-  const startAt = oldOperations.length ? oldOperations[0].blockHeight || 0 : 0;
+  const startAt = oldOperations.length
+    ? (oldOperations[0].blockHeight || 0) + 1
+    : 0;
 
   const {
     blockHeight,
@@ -43,7 +45,6 @@ export const getAccountShape: GetAccountShape = async (info) => {
       nominations,
     },
   };
-  // console.log("getAccountShape", JSON.stringify(shape, null, 2));
 
   return { ...shape, operations };
 };
